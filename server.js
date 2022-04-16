c2cd8bb6ddd2e8922acda86233dcf62d0497802d
ahmed <saiefeddina@gmail.com>
2022-04-16 02:32:27 +0100

    setting up

---
+++ server.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const path = require('path')
+
+
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+
+// Routes
+
+
+
+
+// Connect to mongodb
+const URI = process.env.MONGODB_LOCAL_URL
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, err => {
+    if(err) throw err;
+    console.log("Connected to DATABASE")
+})
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log('Server is running on port', PORT)
+})
